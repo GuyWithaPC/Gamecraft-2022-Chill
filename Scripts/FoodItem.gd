b674791b297cba1413663bef58a37ffa72333b65
Max Petersen <maxvonpetersen@gmail.com>
2022-12-03 01:04:05 -0700

    got the dragging code working (finally)

--- Scripts/FoodItem.gd
+++ Scripts/FoodItem.gd
@@ -1,48 +1,65 @@
 extends RigidBody2D
 
-class_name FoodItem
-
-var _dragged = false
-var _in_play_area = false
+var dragged = false
+var mouse_inside = false
+var mouse_down = false
+var in_play_area = false
+var velocity = Vector2(0,0)
+onready var _last_position = position
 onready var _play_area = get_tree().get_root().get_child(0).get_node("PlayArea")
+onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	# set up event handling
-	self.connect("input_event",self,"_on_input_event")
+	self.connect("mouse_entered",self,"_on_mouse_entered")
+	self.connect("mouse_exited",self,"_on_mouse_exited")
 	_play_area.connect("body_entered",self,"_on_body_entered")
 	_play_area.connect("body_exited",self,"_on_body_exited")
+	self.set_collision_layer_bit(1,true)
 	self.input_pickable = true
+	self.can_sleep = false
 
 func _process(delta):
-	if _dragged:
-		self.position = get_viewport().get_mouse_position()
-		if _in_play_area:
-			self.scale = Vector2(1.25,1.25)
-			if Input.is_action_just_pressed("left_click"):
-				_dragged = false
-
-func _physics_process(delta):
-	# choose whether to have physics disabled or running
-	if _dragged or !_in_play_area:
-		self.sleeping = true
+	if mouse_inside and Input.is_action_just_pressed("left_click"):
+		dragged = !dragged
+		if !dragged:
+			_last_position = position
+		if dragged:
+			self.set_collision_layer_bit(0,false)
+			self.set_collision_mask_bit(0,false)
+		if !dragged and in_play_area:
+			self.set_collision_layer_bit(0,true)
+			self.set_collision_mask_bit(0,true)
+	
+	if dragged:
+		$Sprite.scale = Vector2(1.1,1.1)
+		$Sprite.self_modulate = Color(1,1,1,0.5)
 	else:
-		self.sleeping = false
+		$Sprite.scale = Vector2(1.0,1.0)
+		$Sprite.self_modulate = Color(1,1,1,1)
+
+func _integrate_forces(state):
+	var lv = state.get_linear_velocity()
+	if !in_play_area:
+		lv = Vector2.ZERO
+		if !dragged:
+			state.transform = Transform2D(state.transform.get_rotation(),_last_position)
+	if dragged:
+		lv = (get_viewport().get_mouse_position() - self.position) * 32
+	state.set_linear_velocity(lv)
 
 # event handling stuff
 
-func _on_input_event(viewport, event, shape_idx):
-	# this event handles getting clicked to begin dragging
-	if event is InputEventMouseButton:
-		if event.button_index == 1 and event.pressed:
-			_dragged = !_dragged
+func _on_mouse_entered():
+	mouse_inside = true
+
+func _on_mouse_exited():
+	mouse_inside = false
 
 func _on_body_entered(body):
-	print(body.name)
 	if body.name == self.name:
-		print("entered play area")
-		_in_play_area = true
+		in_play_area = true
 
 func _on_body_exited(body):
 	if body.name == self.name:
-		print("exited play area")
-		_in_play_area = false
+		in_play_area = false
