c5064ea43b5b159658d0a57722910cdca5065149
Max Petersen <maxvonpetersen@gmail.com>
2022-12-03 11:03:37 -0700

    timer rendering (nothing happens when they run out yet

--- Scripts/FoodItem.gd
+++ Scripts/FoodItem.gd
@@ -5,9 +5,13 @@ var mouse_inside = false
 var mouse_down = false
 var in_play_area = false
 var velocity = Vector2(0,0)
+
 onready var _last_position = position
 onready var _play_area = get_tree().get_root().get_child(0).get_node("PlayArea")
 onready var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+export var default_timer = 5.0
+onready var timer = default_timer
 
 func _ready():
 	# set up event handling
@@ -18,6 +22,16 @@ func _ready():
 	self.set_collision_layer_bit(1,true)
 	self.input_pickable = true
 	self.can_sleep = false
+	# misc
+	$Sprite.show_behind_parent = true
+
+func _draw():
+	var percentage = ((default_timer-timer)/default_timer)
+	var start_position = percentage*360
+	var color = Color(0,1,0)
+	color.h = (1.0/3.0)-(percentage/3.0)
+	if (timer/default_timer) < 1:
+		draw_circle_arc_poly(Vector2.ZERO,40,start_position,360,color)
 
 func _process(delta):
 	if mouse_inside and Input.is_action_just_pressed("left_click"):
@@ -37,6 +51,18 @@ func _process(delta):
 	else:
 		$Sprite.scale = Vector2(1.0,1.0)
 		$Sprite.self_modulate = Color(1,1,1,1)
+	
+	if !in_play_area:
+		timer -= delta
+		update()
+	else:
+		if dragged:
+			timer -= delta
+			update()
+		else:
+			if timer < default_timer:
+				timer += 2*delta
+			update()
 
 func _integrate_forces(state):
 	var lv = state.get_linear_velocity()
@@ -63,3 +89,16 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.name == self.name:
 		in_play_area = false
+
+# helper functions
+
+func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
+	var nb_points = 32
+	var points_arc = PoolVector2Array()
+	points_arc.push_back(center)
+	var colors = PoolColorArray([color])
+
+	for i in range(nb_points + 1):
+		var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+	draw_polygon(points_arc, colors)
